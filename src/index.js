73f391a429bbf3e7b56e0ddae794970d21ead594
Sam <sammintz1993@gmail.com>
2018-06-14 13:22:43 -0500

    downloaded files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Create a new component 
+// This component should produce some HTML 
+
+const App = function(){
+	//jsx
+	return <div>Hi</div>
+}
+
+
+// Take this component's HTML and put it on the page (in the dom)
+
