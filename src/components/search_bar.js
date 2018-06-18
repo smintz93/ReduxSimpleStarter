0d029d2a77f5c1f5417bb6b0da200d13eead3663
Sam <sammintz1993@gmail.com>
2018-06-18 12:41:18 -0500

    added state to search bar comp

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,10 +2,24 @@
 import React, { Component } from 'react';
 
 	class SearchBar extends Component {
-		render() {
-			return  <input />
-
+		// first and only function called auto when a new instance of class is created 
+		constructor(props) {
+			super(props);
+			// only inside constructor function can you manuliplate like this with = 
+			this.state = { term: '' }
 		}
+		render() {
+			return (
+				<div>
+					<input onChange={ event => this.setState({term: event.target.value})} />
+					Value of the input: {this.state.term}
+				</div>
+			);
+			
+		}
+
+
+
 	}
 
 export default SearchBar;
