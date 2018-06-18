9840398b7819a49136a0c55e756084b46487dd3f
Sam <sammintz1993@gmail.com>
2018-06-18 10:47:28 -0500

    created components folder with files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 // Create a new component 
 // This component should produce some HTML 
 
-const App = function(){
+const App = () => {
 	//jsx
 	// webpack babel translates jsx so browser can read it. 
 	return <div>Hi</div>
@@ -13,4 +13,5 @@ const App = function(){
 // Take this component's HTML and put it on the page (in the dom)
 
 // making an instance of a component
+// rendering it to the div container in index.html 
 ReactDOM.render(<App />, document.querySelector('.container'));
