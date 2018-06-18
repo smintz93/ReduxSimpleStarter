998992954b896d9be2470b864d81403419e7f60f
Sam <sammintz1993@gmail.com>
2018-06-18 10:37:24 -0500

    displaying message on browser

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 // Create a new component 
 // This component should produce some HTML 
 
 const App = function(){
 	//jsx
+	// webpack babel translates jsx so browser can read it. 
 	return <div>Hi</div>
 }
 
 
 // Take this component's HTML and put it on the page (in the dom)
 
+// making an instance of a component
+ReactDOM.render(<App />, document.querySelector('.container'));
